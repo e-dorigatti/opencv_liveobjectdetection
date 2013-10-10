ba3ba4c9b7e7ebe9b86479bb4dca94e41cec4ce4
emilio <emilio.dorigatti@gmail.com>
2013-10-10 15:43:03 +0200

    live descriptors calculation from cam

--- objdet.py
+++ objdet.py
@@ -2,10 +2,33 @@ import cv
 import cv2
 import numpy as np
 
-def calculateKeyPoints(image):     
-    surf = cv2.SURF(3000)
-    keyp = surf.detect(image, None)
-    return keyp
+def liveDescriptors():
+    capture = cv2.VideoCapture(0)
+    count = 0
+    while count < 250:
+        ret, frame = capture.read()
+        if ret == False:
+            break
+
+        img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        keypoints, d = calculateDescriptors(img)
+
+        for kp in keypoints:
+            p = int(kp.pt[0]), int(kp.pt[1])
+            cv2.circle(frame, p, int(kp.size), (0, 255, 255))
+        
+        cv.ShowImage('img', cv.fromarray(frame))
+        cv.WaitKey(2)
+        
+        count += 1
+
+    capture.release()
+    cv2.destroyWindow('img')
+
+def calculateDescriptors(image, threshold = 3000):
+    surf = cv2.SURF(threshold)
+    kp, descriptors = surf.detectAndCompute(image, None)
+    return kp, descriptors
 
 def showKeyPoints(image, keypoints):
     window = cv2.namedWindow('image')
@@ -25,15 +48,8 @@ if __name__ == '__main__':
     img = cv2.imread('object.jpg', cv.CV_LOAD_IMAGE_GRAYSCALE)
 
     print 'looking for keypoints...'
-    keyp = calculateKeyPoints(img)
+    keyp, descr = calculateDescriptors(img)
     print len(keyp), ' found'
     
-    showKeyPoints(img, keyp)
-
-#capture = cv.CaptureFromCAM(0)
-#count = 0
-#while count < 250:
-#    image = cv.QueryFrame(capture)
-#    cv.ShowImage('Image Window', image)
-#    cv.WaitKey(2)
-#    count += 1
+    # showKeyPoints(img, keyp)
+    liveDescriptors()
