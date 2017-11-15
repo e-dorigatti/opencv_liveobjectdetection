51eee4d2c90f73d1115b7f5f9086ed0a77c84ac8
e-dorigatti <emilio.dorigatti@gmail.com>
2017-11-16 00:11:54 +0100

    converted to opencv 3

--- objdet.py
+++ objdet.py
@@ -1,20 +1,22 @@
-import cv
 import cv2
 import numpy as np
 from sys import argv
 
 class ObjectDetector:
     def __init__(self, obj):
-        self.surf = cv2.SURF()
+        self.surf = cv2.xfeatures2d.SURF_create()
 
-        self.obj_image = cv2.imread(obj, cv.CV_LOAD_IMAGE_GRAYSCALE)
-        self.obj_image = cv2.resize(self.obj_image, (0, 0), fx=0.40, fy=0.40)
-        self.obj_keypoints, self.obj_descriptors = self.surf.detectAndCompute(self.obj_image, None)
+        self.obj_image = cv2.imread(obj, cv2.COLOR_BGR2GRAY)
+        self.obj_image = cv2.resize(self.obj_image, (0, 0), fx=0.05, fy=0.05)
+        self.obj_keypoints, self.obj_descriptors = self.surf.detectAndCompute(
+            self.obj_image, None
+        )
 
-        classes = np.arange(len(self.obj_keypoints), dtype = np.float32)
-        self.classifier = cv2.KNearest()
-        self.classifier.train(self.obj_descriptors, classes, isRegression = False)
-    
+        classes = np.arange(len(self.obj_keypoints), dtype=np.float32)
+        self.classifier = cv2.ml.KNearest_create()
+        self.classifier.train(self.obj_descriptors, cv2.ml.ROW_SAMPLE, classes)
+                              #isRegression=False)
+
     def detect_object(self, descriptors):
         """
         Detects similar descriptors in the specified black and white image.
@@ -29,7 +31,9 @@ class ObjectDetector:
             descriptor = np.array(descriptor, np.float32).reshape(1, len(descriptor))
 
             # find the most similar descriptor in self.obj_descriptors
-            retval, results, neigh_resp, dists = self.classifier.find_nearest(descriptor, 2)
+            retval, results, neigh_resp, dists = self.classifier.findNearest(
+                descriptor, 2
+            )
 
             # i.e. the keypoint1 this descriptor is most similar to
             classification = int(results[0][0])
@@ -48,13 +52,13 @@ class ObjectDetector:
         matches: list of tuples --> (object_keypoint_index, frame_keypoint_index)
         frame_keypoints: a list of all keypoints found on the frame
         """
+
         h1, w1 = self.obj_image.shape[:2]
         h2, w2 = frame.shape[:2]
 
-        vis = np.zeros((max(h1, h2), w1 + w2), np.uint8)
+        vis = np.zeros((max(h1, h2), w1 + w2, 3), np.uint8)
         vis[:h1, :w1] = self.obj_image
         vis[:h2, w1:w1+w2] = frame
-        vis = cv2.cvtColor(vis, cv2.COLOR_GRAY2BGR)
 
         for kp1, kp2 in matches:
             c = (255,255,255)
@@ -77,7 +81,8 @@ class ObjectDetector:
         Processes the specified frame finding matching descriptors and returning
         and image showing them.
         """
-        keypoints, descriptors = self.surf.detectAndCompute(image, None)
+        black_white = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        keypoints, descriptors = self.surf.detectAndCompute(black_white, None)
         matches = self.detect_object(descriptors)
         return self.draw_frame(image, matches, keypoints)
 
@@ -86,22 +91,20 @@ def main():
     detector = ObjectDetector('object.jpg')
 
     step, i = 2, 0
-
     while True:
-        ret, frame = capture.read()
-        if ret == False:
-            break
-
-        img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
         i += 1
-        if i % step != 0:
-            continue
 
-        final = detector.process_frame(img)
+        ret, frame = capture.read()
+        assert ret
 
-        cv2.imshow('img', final)
-        cv.WaitKey(2)
+        frame = frame.copy()[:, ::-1]
+
+        processed = detector.process_frame(frame)
+        cv2.imshow('img', processed)
+
+        k = cv2.waitKey(1) & 0xff
+        if k == ord('q'):
+            break
 
     capture.release()
     cv2.destroyWindow('img')
